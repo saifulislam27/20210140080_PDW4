37c5dbff857920e9abb4bcd071389a5c58287e1a
saifulislam27 <mariyatilambeja@gmail.com>
2024-03-20 22:19:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function show(){
+    alert("UDAH DONE")
+}
+Date()
+
+let tanggal = new Date(
+    document.getElementById("tanggal").innerHTML= tanggal
+)
+function tampil(){
+    alert("Ini adalah Button")
+}
